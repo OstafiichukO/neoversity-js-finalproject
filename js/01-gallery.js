1aca38550baeee21a90bce02169a62163c582724
OstafiichukO <issimo333@gmail.com>
2023-09-11 21:00:21 +0300

    feat: finel project

---
+++ js/01-gallery.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import { galleryItems } from './gallery-items.js';
+
+const imgContainer = document.querySelector('.gallery');
+let preview = null;
+
+const galleryMarkup = createMarkup(galleryItems);
+imgContainer.innerHTML = galleryMarkup;
+
+function createMarkup(galleryData) {
+    return galleryData
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+        `;
+        })
+        .join('');
+}
+
+function onClickImg(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') return;
+
+    createBasicLightbox(event.target);
+    openModal();
+}
+
+function createBasicLightbox(img) {
+    preview = basicLightbox.create(
+        `
+        <img src="${img.getAttribute('data-source')}" width="800" height="600">
+    `,
+        {
+            onShow: () => {
+                window.addEventListener('keydown', closeOnEscPress);
+            },
+            onClose: () => {
+                window.removeEventListener('keydown', closeOnEscPress);
+            },
+        },
+    );
+}
+
+function openModal() {
+    if (!preview) return;
+    preview.show();
+}
+
+function closeOnEscPress(event) {
+    if (event.code === 'Escape' && preview) {
+        preview.close();
+    }
+}
+
+imgContainer.addEventListener('click', onClickImg);
